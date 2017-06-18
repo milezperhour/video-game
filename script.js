57b1e7edb8c9c34b5043886875963a53c650b901
milezperhour <hyperbolvoid@gmail.com>
2017-06-18 15:30:59 -0700

    distance and collision detection updated

--- script.js
+++ script.js
@@ -15,35 +15,32 @@ var player = {
 
 var enemyList = {};
 
-var enemy = {
-    x: 150,
-    spdX: 10,
-    y: 350,
-    spdY: 15,
-    name: 'E',
-    id: 'E1'
-};
-enemyList['E1'] = enemy
+Enemy('E1', 150, 350, 10, 15);
+Enemy('E2', 250, 350, 10, -15);
+Enemy('E3', 250, 150, 10, -8);
 
-var enemy2 = {
-    x: 250,
-    spdX: 10,
-    y: 350,
-    spdY: -15,
-    name: 'E',
-    id: 'E2'
-};
-enemyList['E2'] = enemy2;
+function getDistanceBetweenEntity(entity1, entity2) {
+    var vx = entity1.x - entity2.x;
+    var vy = entity1.y - entity2.y;
+    return Math.sqrt(vx*vx + vy*vy);
+}
 
-var enemy3 = {
-    x: 11,
-    spdX: 10,
-    y: 350,
-    spdY: 5,
-    name: 'E',
-    id: 'E3'
-};
-enemyList['E3'] = enemy3;
+function testCollisionEntity(entity1, entity2){
+    var distance = getDistanceBetweenEntity(entity1, entity2);
+    return distance < 30;
+}
+
+function Enemy(id, x, y, spdX, spdY){
+    var enemy3 = {
+        x: x,
+        spdX: spdX,
+        y: y,
+        spdY: spdY,
+        name: 'E',
+        id: id
+    };
+    enemyList[id] = enemy3;
+}
 
 setInterval(update, 40);
 
@@ -64,9 +61,15 @@ function updateEntity(entity){
 
 function update() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    updateEntity(player);
 
     for (var i in enemyList){
         updateEntity(enemyList[i]);
+
+        var isColliding = testCollisionEntity(player, enemyList[i]);
+        if(isColliding) {
+            console.log('Colliding!')
+        }
     }
+
+    updateEntity(player);
 }
